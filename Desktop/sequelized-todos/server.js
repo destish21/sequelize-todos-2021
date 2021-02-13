4e7b15000e02ab5f64fb1c533e8d154e166b9ae0
desta mulu <destish21@yahoo.com>
2021-02-13 11:27:02 -0600

    first commit

---
+++ Desktop/sequelized-todos/server.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const db = require("./models")
+const PORT = 3000
+const app = express()
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"))
+
+db.sequelize.sync().then(()=>{
+    app.listen(PORT, ()=> {
+        console.log("App listening on PORT " + PORT);
+      });
+      
+})
+
